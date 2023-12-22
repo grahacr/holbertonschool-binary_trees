eb04c424eccc0e9e8b11187e6e6595260e69b9ff
Courtney Graham <7194@holbertonstudents.com>
2023-12-22 13:32:35 -0800

    fix output

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,7 +9,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new = malloc(sizeof(binary_tree_t));
 
-	if(!new || !parent)
+	if(new == NULL || parent == NULL)
 		return(NULL);
 	new->n = value;
 	new->left = new->right = NULL;
