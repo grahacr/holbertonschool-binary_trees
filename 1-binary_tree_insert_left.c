0041764318ced37399f3c9bd135f949f99a1d0f7
Courtney Graham <7194@holbertonstudents.com>
2023-12-22 13:38:49 -0800

    fix memory problem

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,14 +9,16 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new = malloc(sizeof(binary_tree_t));
 
-	if(new == NULL || parent == NULL)
+	if (new == NULL || parent == NULL)
 		return(NULL);
 	new->n = value;
+	new->parent = parent;
 	new->left = new->right = NULL;
-	if(parent->left != NULL)
+	if (parent->left != NULL)
 	{
 		new->left = parent->left;
+		new->left->parent = new;
 	}
-	parent->left=new;
-	return(new);
+	parent->left = new;
+	return (new);
 }
