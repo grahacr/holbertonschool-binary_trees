32596ac385f272cf292c2d8b1cd3202baf3ef346
Courtney Graham <7194@holbertonstudents.com>
2023-12-22 13:30:35 -0800

    fix semicolon

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,7 +10,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *new = malloc(sizeof(binary_tree_t));
 
 	if(!new || !parent)
-		return(NULL)
+		return(NULL);
 	new->n = value;
 	new->left = new->right = NULL;
 	if(parent->left != NULL)
