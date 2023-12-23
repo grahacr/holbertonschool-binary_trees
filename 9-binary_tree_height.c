5cd2b235fac2264b3e4f090cd09a565abb58f956
Courtney Graham <7194@holbertonstudents.com>
2023-12-22 18:10:23 -0800

    fix organization for compilation

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,10 +6,12 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left, right;
+
 	if (tree == NULL)
 		return(0);
-	size_t left = binary_tree_height(tree->left);
-	size_t right = binary_tree_height(tree->right);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
 
-	return(lef > right) ? (left + 1) : (right + 1);
+	return (left > right) ? (left + 1) : (right + 1);
 }
