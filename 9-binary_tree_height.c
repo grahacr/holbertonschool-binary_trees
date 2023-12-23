927fef14ba5bb8b7c419bb6514fbe0f3a328d925
Courtney Graham <7194@holbertonstudents.com>
2023-12-22 18:29:27 -0800

    fix typo

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,7 +10,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	if (tree->next != NULL)
+	if (tree->left != NULL)
 		left = 1 + binary_tree_height(tree->left);
 	if (tree->right != NULL)
 		right = 1 + binary_tree_height(tree->right);
