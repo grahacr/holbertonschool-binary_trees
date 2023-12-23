deda1fe50f6e1d434c4282640fa38e2ae657e2fc
Courtney Graham <7194@holbertonstudents.com>
2023-12-22 19:59:16 -0800

    bracket

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,5 +10,5 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (0);
 	else
-		return (binary_tree_sibling(node->parent);
+		return (binary_tree_sibling(node->parent));
 }
