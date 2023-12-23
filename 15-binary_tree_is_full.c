b2f734e1f89ab0ef0cecc4bcc5c73b26ff88f440
Courtney Graham <7194@holbertonstudents.com>
2023-12-22 20:01:10 -0800

    fix betty for 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,7 +8,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (1);
-	if ((tree->left == NULL && tree->right != NULL) || (tree->left != NULL && tree->right == NULL))
+	if ((!tree->left && tree->right != NULL) || (tree->left != NULL && !tree->right))
 		return (0);
 	return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 }
