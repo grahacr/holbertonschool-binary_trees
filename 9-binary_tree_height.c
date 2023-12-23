b581ba27f436cf28d6f5554b23a78301ccd7c745
Courtney Graham <7194@holbertonstudents.com>
2023-12-22 18:31:39 -0800

    fix output

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,7 +6,8 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left, right;
+	size_t left = 0;
+	size_t right = 0;
 
 	if (tree == NULL)
 		return (0);
@@ -14,5 +15,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		left = 1 + binary_tree_height(tree->left);
 	if (tree->right != NULL)
 		right = 1 + binary_tree_height(tree->right);
-	return (left > right) ? (left) : (right);
+	return ((left > right) ? (left) : (right));
 }
